f05c4d8db9676bf6316ccb4405eef1681e423024
Vipul Mehra <vipulmeh23@gmail.com>
2023-08-24 01:07:49 -0700

    Customizing JSON output in this one using JSONB

--- rest-book/src/main/java/org/agoncal/quarkus/microservices/book/Book.java
+++ rest-book/src/main/java/org/agoncal/quarkus/microservices/book/Book.java
@@ -1,14 +1,21 @@
 package org.agoncal.quarkus.microservices.book;
 
+import jakarta.json.bind.annotation.JsonbDateFormat;
+import jakarta.json.bind.annotation.JsonbProperty;
+
 import java.time.Instant;
 
 public class Book {
 
+    @JsonbProperty("isbn_13")
     public String isbn13;
     public String title;
     public String author;
+    @JsonbProperty("year_of_publication")
     public int yearOfPublication;
     public String genre;
+    @JsonbDateFormat("yyyy-MM-dd")
+    @JsonbProperty("creation_date")
     public Instant creationDate;
 
     @Override
