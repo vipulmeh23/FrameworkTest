d90a7405ac4f13a91fbdd06131402cb37cac3c02
Vipul Mehra <vipulmeh23@gmail.com>
2023-08-24 00:58:31 -0700

    Exposing the book endpoint

--- rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
@@ -1,16 +1,36 @@
 package org.agoncal.quarkus.microservices.book;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.jboss.logging.Logger;
+
+import java.time.Instant;
 
 @Path("/api/books")
 public class BookResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Inject
+    Logger logger;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createBook(
+            @FormParam("title") String title,
+            @FormParam("author") String author,
+            @FormParam("year") int yearOfPublication,
+            @FormParam("genre") String genre
+    ) {
+        Book book = new Book();
+        book.isbn13 = "We will get it later from the Number microservices";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();
+        logger.info("Book created: " + book);
+        return Response.status(201).entity(book).build();
     }
 }
