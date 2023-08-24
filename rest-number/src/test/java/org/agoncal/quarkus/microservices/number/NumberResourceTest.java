ec406638e5fa809511cb7d62efd6aea7635afabc
Vipul Mehra <vipulmeh23@gmail.com>
2023-08-23 19:51:23 -0700

    Unit testing with Quarkus

--- rest-number/src/test/java/org/agoncal/quarkus/microservices/number/NumberResourceTest.java
+++ rest-number/src/test/java/org/agoncal/quarkus/microservices/number/NumberResourceTest.java
@@ -1,21 +1,30 @@
 package org.agoncal.quarkus.microservices.number;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
+import static com.google.inject.matcher.Matchers.not;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.Matchers.hasKey;
 
 @QuarkusTest
 public class NumberResourceTest {
 
     @Test
     public void testHelloEndpoint() {
-        given()
-          .when().get("/api/numbers")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+        ValidatableResponse z = given()
+                .when().get("/api/numbers")
+                .then()
+                .statusCode(200)
+                .body("isbn_13", startsWith("13-"))
+                .body("isbn_10", startsWith("10-"))
+                .body(Matchers.not(hasKey("generationDate")));
     }
 
 }
